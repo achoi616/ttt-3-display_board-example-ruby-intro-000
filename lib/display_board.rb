0b54e7d3c07c5a5b7f54f67e61f059d5c934e36e
achoi616 <achoi616@gmail.com>
2020-06-04 19:43:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 def display_board
   puts "Welcome to Tic Tac Toe"
-  puts "   ""|""   ""|""   "
-  puts "-----------"
-  puts "   ""|""   ""|""   "
-  puts "-----------"
-  puts "   ""|""   ""|""   "
+  puts row = ["   ""|""   ""|""   "]
+  puts separator = ["-----------"]
+  puts row = ["   ""|""   ""|""   "]
+  puts separator = ["-----------"]
+  puts row = ["   ""|""   ""|""   "]  
 end
 display_board
+
